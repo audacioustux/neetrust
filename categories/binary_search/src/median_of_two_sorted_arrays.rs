8f2db3f19f50b25db43876150c28be890244d7cb
Audacious Tux <tangimhossain1@gmail.com>
2023-08-24 12:28:17 +0000

    log(n) solution for median_of_two_sorted_array

--- categories/binary_search/src/median_of_two_sorted_arrays.rs
+++ categories/binary_search/src/median_of_two_sorted_arrays.rs
@@ -2,27 +2,33 @@ pub struct Solution;
 
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        let len = nums1.len() + nums2.len();
-        let mut sorted = Vec::with_capacity(len / 2);
-
-        let (mut i, mut j) = (0, 0);
-        while i < nums1.len() && j < nums2.len() && i + j <= len / 2 {
-            if nums1[i] < nums2[j] {
-                sorted.push(nums1[i]);
-                i += 1;
-            } else {
-                sorted.push(nums2[j]);
-                j += 1;
-            }
+        if nums1.len() > nums2.len() {
+            return Solution::find_median_sorted_arrays(nums2, nums1);
         }
-        let (i, j) = (i.min(nums1.len()), j.min(nums2.len()));
-        sorted.extend_from_slice(&nums1[i..]);
-        sorted.extend_from_slice(&nums2[j..]);
 
-        if len % 2 == 0 {
-            (sorted[len / 2 - 1] + sorted[len / 2]) as f64 / 2.0
-        } else {
-            sorted[len / 2] as f64
+        let (len1, len2) = (nums1.len(), nums2.len());
+        let total_len = len1 + len2;
+        let (mut left, mut right) = (0, len1);
+        loop {
+            let mid1 = left + (right - left) / 2;
+            let mid2 = (total_len + 1) / 2 - mid1;
+            let (l1, r1, l2, r2) = (
+                if mid1 == 0 { i32::MIN } else { nums1[mid1 - 1] },
+                if mid1 == len1 { i32::MAX } else { nums1[mid1] },
+                if mid2 == 0 { i32::MIN } else { nums2[mid2 - 1] },
+                if mid2 == len2 { i32::MAX } else { nums2[mid2] },
+            );
+            if l1 <= r2 && l2 <= r1 {
+                if total_len % 2 == 0 {
+                    return (l1.max(l2) as f64 + r1.min(r2) as f64) / 2.0;
+                } else {
+                    return l1.max(l2) as f64;
+                }
+            } else if l1 > r2 {
+                right = mid1 - 1;
+            } else {
+                left = mid1 + 1;
+            }
         }
     }
 }

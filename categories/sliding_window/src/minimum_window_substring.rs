503a6204c099f419f523c79841edead8939b3bc0
Audacious Tux <tangimhossain1@gmail.com>
2023-08-27 10:17:12 +0000

    refactor minimum window substring

--- categories/sliding_window/src/minimum_window_substring.rs
+++ categories/sliding_window/src/minimum_window_substring.rs
@@ -2,27 +2,38 @@ pub struct Solution;
 
 impl Solution {
     pub fn min_window(s: String, t: String) -> String {
-        use std::collections::HashMap;
-        let mut map = HashMap::new();
-        t.bytes().for_each(|b| *map.entry(b).or_insert(0) += 1);
-
-        let mut res: Option<&[u8]> = None;
+        let s = s.as_bytes();
+        let t = t.as_bytes();
+        if s.len() < t.len() {
+            return "".to_string();
+        }
+        let mut window = [0; 128];
+        let (mut have, need) = (
+            0,
+            t.iter().fold([0; 128], |mut acc, b| {
+                acc[*b as usize] += 1;
+                acc
+            }),
+        );
         let mut left = 0;
-        let s = s.into_bytes();
-
-        for (end, &end_char) in s.iter().enumerate() {
-            map.entry(end_char).and_modify(|n| *n -= 1);
-            if res.is_some() || map.values().all(|n| *n <= 0) {
-                while map.get(&s[left]).copied().unwrap_or(-1) < 0 {
-                    map.entry(s[left]).and_modify(|n| *n += 1);
-                    left += 1;
+        let mut res: Option<&[u8]> = None;
+        for (right, &b) in s.iter().enumerate() {
+            window[b as usize] += 1;
+            if window[b as usize] <= need[b as usize] {
+                have += 1;
+            }
+            while have == t.len() {
+                if res.is_none() || res.as_ref().unwrap().len() > right - left + 1 {
+                    res = Some(&s[left..=right]);
                 }
-                if res.is_none() || res.unwrap().len() > end - left {
-                    res = Some(&s[left..=end]);
+                window[s[left] as usize] -= 1;
+                if window[s[left] as usize] < need[s[left] as usize] {
+                    have -= 1;
                 }
+                left += 1;
             }
         }
-        String::from_utf8(res.unwrap_or_default().to_vec()).unwrap()
+        res.map_or("".to_string(), |s| String::from_utf8(s.to_vec()).unwrap())
     }
 }
 
@@ -43,5 +54,9 @@ fn test_min_window() {
     assert_eq!(
         Solution::min_window("ab".to_string(), "a".to_string()),
         "a".to_string()
+    );
+    assert_eq!(
+        Solution::min_window("a".to_string(), "b".to_string()),
+        "".to_string()
     );
 }

203798f152bb652f351c1b7d752455ff59d4ce30
Audacious Tux <tangimhossain1@gmail.com>
2023-08-27 13:31:15 +0000

    vecdeque sliding window maximum

--- categories/sliding_window/src/sliding_window_maximum.rs
+++ categories/sliding_window/src/sliding_window_maximum.rs
@@ -3,20 +3,41 @@ pub struct Solution;
 impl Solution {
     pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
         use std::collections::VecDeque;
-        let mut result = Vec::new();
-        let mut deque = VecDeque::new();
-        for i in 0..nums.len() {
-            while !deque.is_empty() && nums[*deque.back().unwrap()] < nums[i] {
-                deque.pop_back();
-            }
-            deque.push_back(i);
-            if i >= k as usize - 1 {
-                result.push(nums[*deque.front().unwrap()]);
-                if *deque.front().unwrap() == i + 1 - k as usize {
+        let k = k as usize;
+        let mut res = Vec::with_capacity(nums.len() - k + 1);
+        let mut deque = VecDeque::with_capacity(k);
+
+        let mut left = 0;
+        for (right, &num) in nums.iter().enumerate() {
+            // remove out of range
+            if let Some(&idx) = deque.front() {
+                if idx < left {
                     deque.pop_front();
                 }
             }
+            // remove smaller
+            while deque.back().map_or(false, |&idx| nums[idx] < num) {
+                deque.pop_back();
+            }
+            // push current
+            deque.push_back(right);
+
+            let window_size = right - left + 1;
+            // if window is full
+            if window_size == k {
+                res.push(nums[deque.front().unwrap().clone()]); // max is the first
+                left += 1; // move left cursor to right
+            }
         }
-        result
+        res
     }
+}
+
+#[test]
+fn test_max_sliding_window() {
+    assert_eq!(
+        Solution::max_sliding_window(vec![1, 3, -1, -3, 5, 3, 6, 7], 3),
+        vec![3, 3, 5, 5, 6, 7]
+    );
+    assert_eq!(Solution::max_sliding_window(vec![1, -1], 1), vec![1, -1]);
 }
